ae03a82af7c3a8e5b626dcb188af6328fac2d2be
Twan Goosen <twan@clarin.eu>
2017-06-22 14:19:39 +0200

    VLO version to 4.2.0-beta1

--- copy_data.sh
+++ copy_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VLO_VERSION=4.1.0-beta1
+VLO_VERSION=4.2.0-beta1
 REMOTE_RELEASE_URL="https://github.com/clarin-eric/VLO/releases/download/vlo-${VLO_VERSION}/vlo-${VLO_VERSION}-Distribution.tar.gz"
 NAME="vlo-${VLO_VERSION}"
 
