9844c9d4889136460668203db4aa0bcf4657c4a9
Twan Goosen <twan@clarin.eu>
2017-07-20 11:37:07 +0000

    Vlo 4.2.1 beta

--- image/filter-vlo-config.sh
+++ image/filter-vlo-config.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 CONFIG=$1
 
 if [ -z "$CONFIG" ]
@@ -13,36 +13,25 @@ then
 	exit 1
 fi
 
-if [ -z "$VLO_DOCKER_SOLR_URL" ]
-then
-	echo "Error: VLO_DOCKER_SOLR_URL not set"
-	exit 1
-fi
+SED_COMMAND=""
 
-if [ -z "$VLO_DOCKER_PUBLIC_HOME_URL" ]
-then
-	echo "Error: VLO_DOCKER_PUBLIC_HOME_URL not set"
-	exit 1
-fi
-
-if [ -z "$VLO_DOCKER_MAPPING_BASE_URI" ]
-then
-	echo "Error: VLO_DOCKER_MAPPING_BASE_URI not set"
-	exit 1
-fi
+for v in VLO_DOCKER_SOLR_URL \
+		VLO_DOCKER_PUBLIC_HOME_URL \
+		VLO_DOCKER_MAPPING_BASE_URI \
+		VLO_DOCKER_FILE_PROCESSING_THREADS \
+		VLO_DOCKER_SOLR_THREADS
+do
+	echo $v
+	val=$(eval echo \${$v})
+	if [ -z "$val" ]
+	then
+		echo "Error: ${v} not set"
+		exit 1
+	fi
+	echo "\${VLO_DOCKER_SOLR_URL} -> ${val}"
+	SED_COMMAND+="s/\${${v}}/${val//\//\\/}/g;"
+done
 
 echo "Substituting placeholder values in ${CONFIG}"
-
-#VLO_DOCKER_SOLR_URL
-echo "\${VLO_DOCKER_SOLR_URL} -> ${VLO_DOCKER_SOLR_URL}"
-SED_COMMAND="s/\${VLO_DOCKER_SOLR_URL}/${VLO_DOCKER_SOLR_URL//\//\\/}/g;"
-
-#VLO_DOCKER_PUBLIC_HOME_URL
-echo "\${VLO_DOCKER_PUBLIC_HOME_URL} -> ${VLO_DOCKER_PUBLIC_HOME_URL}"
-SED_COMMAND+="s/\${VLO_DOCKER_PUBLIC_HOME_URL}/${VLO_DOCKER_PUBLIC_HOME_URL//\//\\/}/g;"
-
-#VLO_DOCKER_MAPPING_BASE_URI
-echo "\${VLO_DOCKER_MAPPING_BASE_URI} -> ${VLO_DOCKER_MAPPING_BASE_URI}"
-SED_COMMAND+="s/\${VLO_DOCKER_MAPPING_BASE_URI}/${VLO_DOCKER_MAPPING_BASE_URI//\//\\/}/g;"
 
 sed -i -e $SED_COMMAND $CONFIG
