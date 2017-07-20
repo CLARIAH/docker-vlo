00b83c85ffa1d3bea16c08a0b0f30df34f126e72
Willem Elbers <wjm.elbers@gmail.com>
2017-07-20 19:53:04 +0200

    Using fluentd enabled base image and set basic config values

--- image/init.sh
+++ image/init.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+SOLR_DATA=${_SOLR_DATA:-"/opt/solr-data"}
+VLO_DOCKER_SOLR_URL=${_VLO_DOCKER_SOLR_URL:-"http://localhost:8080/solr/core0/"}
+VLO_DOCKER_PUBLIC_HOME_URL=${_VLO_DOCKER_PUBLIC_HOME_URL:-"http://localhost"}
+VLO_DOCKER_MAPPING_BASE_URI=${_VLO_DOCKER_MAPPING_BASE_URI:-"file:/srv/VLO-mapping/"}
+VLO_DOCKER_FILE_PROCESSING_THREADS=${_VLO_DOCKER_FILE_PROCESSING_THREADS:-"-1"}
+VLO_DOCKER_SOLR_THREADS=${_VLO_DOCKER_SOLR_THREADS:-"4"}
+
+export "VLO_DOCKER_SOLR_URL=${VLO_DOCKER_SOLR_URL}"
+export "VLO_DOCKER_PUBLIC_HOME_URL=${VLO_DOCKER_PUBLIC_HOME_URL}"
+export "VLO_DOCKER_MAPPING_BASE_URI=${VLO_DOCKER_MAPPING_BASE_URI}"
+export "VLO_DOCKER_FILE_PROCESSING_THREADS=${VLO_DOCKER_FILE_PROCESSING_THREADS}"
+export "VLO_DOCKER_SOLR_THREADS=${VLO_DOCKER_SOLR_THREADS}"
+
 /bin/bash /opt/filter-vlo-config.sh /opt/vlo/config/VloConfig.xml
 
 #Update mapping definitions
-cd /srv/VLO-mapping && \
+cd /srv/VLO-mapping/ && \
 curl -L "https://github.com/clarin-eric/VLO-mapping/archive/beta.tar.gz" | tar zxvf - --strip-components=1
