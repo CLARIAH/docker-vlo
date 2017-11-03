f61495f553b037c004e44506c65db7fe7e733495
Twan Goosen <twan@clarin.eu>
2017-11-03 15:25:56 +0100

    Filtering of sitemap generator properties; also changed statsd prefix in stats config template

--- image/importer.sh
+++ image/importer.sh
@@ -4,6 +4,11 @@ if [ ! -z "${STATSD_PREFIX}" ]; then
     sed -i "s/report\.statsd\.prefix\=.*/report\.statsd\.prefix\=${STATSD_PREFIX}/g" /opt/vlo/bin/statistics/config.properties
 fi
 
+# Filter sitemap properties
+sed -e "s@^VLO_URL\=.*@VLO_URL\=${VLO_DOCKER_PUBLIC_HOME_URL}\/@g" \
+		-e "s@^SOLR_QUERY_URL\=.*@SOLR_QUERY_URL\=${VLO_DOCKER_SOLR_URL}select\?@g" \
+		-i /opt/vlo/bin/sitemap-generator/config.properties
+
 #Update mapping definitions
 if [ ! -z "${VLO_MAPPING_DEFINITIONS_DIST_URL}" ]; then
 	cd "${VLO_MAPPING_DEFINITIONS_DIR}" && \
