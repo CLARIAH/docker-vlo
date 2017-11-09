5126e88aca26a51efe7d1c21f6df0ad0dc9b4323
Twan Goosen <twan@clarin.eu>
2017-11-09 16:14:55 +0100

    vlo importer script logging, followed with fluentd

--- image/importer.sh
+++ image/importer.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+IMPORTER_SCRIPT_LOG_FILE="/var/log/vlo-import-script.log"
+
+touch $IMPORTER_SCRIPT_LOG_FILE
+
 if [ ! -z "${STATSD_PREFIX}" ]; then
-    echo "Adjusting with statsd prefix (${STATSD_PREFIX}) override"
+    echo "Adjusting with statsd prefix (${STATSD_PREFIX}) override" > $IMPORTER_SCRIPT_LOG_FILE
     sed -i "s/report\.statsd\.prefix\=.*/report\.statsd\.prefix\=${STATSD_PREFIX}/g" /opt/vlo/bin/statistics/config.properties
 fi
 
@@ -14,19 +18,17 @@ if [ ! -z "${VLO_MAPPING_DEFINITIONS_DIST_URL}" ]; then
 	cd "${VLO_MAPPING_DEFINITIONS_DIR}" && \
 	curl -L "${VLO_MAPPING_DEFINITIONS_DIST_URL}" | tar zxvf - --strip-components=1
 else
-	echo "Not retrieving (updated) VLO mapping definitions!"
+	echo "Not retrieving (updated) VLO mapping definitions!" > $IMPORTER_SCRIPT_LOG_FILE
 fi
 
 #Run importer
 cd /opt/vlo/bin/ && \
-sh vlo_solr_importer.sh
+sh vlo_solr_importer.sh > $IMPORTER_SCRIPT_LOG_FILE 2>&1
 
 #Generate statistics
 cd /opt/vlo/bin/statistics/ && \
-sh start.sh /opt/vlo/bin/statistics/config.properties
+sh start.sh /opt/vlo/bin/statistics/config.properties >> $IMPORTER_SCRIPT_LOG_FILE 2>&1
 
 #Generate sitemap
 cd /opt/vlo/bin/sitemap-generator/ && \
-sh start.sh
-
-
+sh start.sh >> $IMPORTER_SCRIPT_LOG_FILE 2>&1
