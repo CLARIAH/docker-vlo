e17c5d38bdf4968301c64a9b726c7d60578dabf4
Twan Goosen <twan@clarin.eu>
2017-11-23 09:56:16 +0100

    Replace filter-vlo*.sh with a more generic filter-config-file.sh

--- image/init.sh
+++ image/init.sh
@@ -2,8 +2,22 @@
 set -e
 
 # Filter VLO configuration
-/bin/bash /opt/filter-vlo-config.sh ${VLO_DOCKER_CONFIG_FILE}
-/bin/bash /opt/filter-vlo-context.sh /srv/tomcat8/conf/Catalina/localhost/ROOT.xml
+/bin/bash /opt/filter-config-file.sh ${VLO_DOCKER_CONFIG_FILE} \
+	VLO_DOCKER_SOLR_URL \
+	VLO_DOCKER_PUBLIC_HOME_URL \
+	VLO_DOCKER_MAPPING_BASE_URI \
+	VLO_DOCKER_FILE_PROCESSING_THREADS \
+	VLO_DOCKER_SOLR_THREADS \
+	VLO_DOCKER_DELETE_ALL_FIRST \
+	VLO_DOCKER_MAX_DAYS_IN_SOLR \
+	VLO_DOCKER_DATAROOTS_FILE
+
+/bin/bash /opt/filter-config-file.sh /srv/tomcat8/conf/Catalina/localhost/ROOT.xml \
+	VLO_DOCKER_CONFIG_FILE \
+	VLO_DOCKER_PIWIK_ENABLE_TRACKER \
+	VLO_DOCKER_PIWIK_SITE_ID \
+	VLO_DOCKER_PIWIK_HOST \
+	VLO_DOCKER_PIWIK_DOMAINS
 
 # Update mapping definitions
 if [ ! -z "${VLO_MAPPING_DEFINITIONS_DIST_URL}" ]; then
