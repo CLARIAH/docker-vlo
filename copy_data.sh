9e77f72dc601f7396761b29f92fe906c3669a737
Twan Goosen <twan@clarin.eu>
2018-02-05 09:35:57 +0100

    VLO version to 4.3.4-RC1

--- copy_data.sh
+++ copy_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VLO_VERSION=4.3.3
+VLO_VERSION=4.3.4-RC1
 REMOTE_RELEASE_URL="https://github.com/clarin-eric/VLO/releases/download/vlo-${VLO_VERSION}/vlo-${VLO_VERSION}-docker.tar.gz"
 NAME="vlo-${VLO_VERSION}-docker"
 VLO_DISTR_FILE="webapp/vlo-${VLO_VERSION}-docker.tar.gz"
