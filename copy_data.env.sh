e865ef7aef0282c11da22b10cbccfaf6cdc31345
Twan Goosen <twan@clarin.eu>
2018-02-15 10:18:47 +0100

    Copy data script reads copy_data.env.sh file or optionally a different ${ENV_FILE}. Added a development alternative

---
+++ copy_data.env.sh
@@ -0,0 +1,5 @@
+VLO_VERSION=4.3.5
+REMOTE_RELEASE_URL="https://github.com/clarin-eric/VLO/releases/download/vlo-${VLO_VERSION}/vlo-${VLO_VERSION}-docker.tar.gz"
+NAME="vlo-${VLO_VERSION}-docker"
+VLO_DISTR_FILE="webapp/vlo-${VLO_VERSION}-docker.tar.gz"
+VLO_DISTR_DIR="webapp/vlo"
---
+++ copy_data_dev.env.sh
@@ -0,0 +1,5 @@
+VLO_VERSION=4.4-SNAPSHOT
+REMOTE_RELEASE_URL="file:/Users/twagoo/git/VLO/vlo-distribution/target/vlo-${VLO_VERSION}-docker.tar.gz"
+NAME="vlo-${VLO_VERSION}-docker"
+VLO_DISTR_FILE="webapp/vlo-${VLO_VERSION}-docker.tar.gz"
+VLO_DISTR_DIR="webapp/vlo"
