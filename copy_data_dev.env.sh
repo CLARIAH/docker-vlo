344ac7553e17ecdb3bf0b64ec762f8b662641185
Twan Goosen <twan@clarin.eu>
2018-05-02 15:51:33 +0200

    dev version to 4.5-SNAPSHOT

--- copy_data_dev.env.sh
+++ copy_data_dev.env.sh
@@ -1,4 +1,4 @@
-VLO_VERSION=4.4.1
+VLO_VERSION=4.5-SNAPSHOT
 REMOTE_RELEASE_URL="file:/Users/twagoo/git/VLO/vlo-distribution/target/vlo-${VLO_VERSION}-docker.tar.gz"
 NAME="vlo-${VLO_VERSION}-docker"
 VLO_DISTR_FILE="webapp/vlo-${VLO_VERSION}-docker.tar.gz"
