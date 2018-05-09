b03205e52dbe9accdf74b333159357eaaa5b9ae8
Twan Goosen <twan@clarin.eu>
2018-05-09 09:40:09 +0200

    VLO version for build to 4.4.3

--- copy_data_dev.env.sh
+++ copy_data_dev.env.sh
@@ -1,5 +1,5 @@
-VLO_VERSION=4.5-SNAPSHOT
-REMOTE_RELEASE_URL="file:/Users/twagoo/git/VLO/vlo-distribution/target/vlo-${VLO_VERSION}-docker.tar.gz"
+VLO_VERSION=4.4.3-SNAPSHOT
+REMOTE_RELEASE_URL="file:/Users/twagoo/Desktop/VLO/vlo-distribution/target/vlo-${VLO_VERSION}-docker.tar.gz"
 NAME="vlo-${VLO_VERSION}-docker"
 VLO_DISTR_FILE="webapp/vlo-${VLO_VERSION}-docker.tar.gz"
 VLO_DISTR_DIR="webapp/vlo"
