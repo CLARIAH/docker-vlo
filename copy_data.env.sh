5ab79c53df0463a44c3cecdddfde5cbcf34202c4
Twan Goosen <twan@clarin.eu>
2019-05-15 14:59:42 +0200

    VLO 4.7.0-beta1

--- copy_data.env.sh
+++ copy_data.env.sh
@@ -1,4 +1,4 @@
-VLO_VERSION="4.6.0"
+VLO_VERSION="4.7.0-beta1"
 REMOTE_RELEASE_URL="https://github.com/clarin-eric/VLO/releases/download/${VLO_VERSION}/vlo-${VLO_VERSION}-docker.tar.gz"
 NAME="vlo-${VLO_VERSION}-docker"
 VLO_DISTR_FILE="webapp/vlo-${VLO_VERSION}-docker.tar.gz"
