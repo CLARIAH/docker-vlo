bb23c2d048d5c9d60a5fbd6305570b931682a31e
Twan Goosen <twan@clarin.eu>
2019-05-28 11:49:19 +0200

    Revert "vlo dev version to 4.8-SNAPSHOT"
    This reverts commit 057ef82747c549fe1294c6c04b8d09f74f00a2aa.

--- copy_data_dev.env.sh
+++ copy_data_dev.env.sh
@@ -1,4 +1,4 @@
-VLO_VERSION="4.8-SNAPSHOT"
+VLO_VERSION="4.7-SNAPSHOT"
 REMOTE_RELEASE_URL="file:/Users/twagoo/git/VLO/vlo-distribution/target/vlo-${VLO_VERSION}-docker.tar.gz"
 NAME="vlo-${VLO_VERSION}-docker"
 VLO_DISTR_FILE="webapp/vlo-${VLO_VERSION}-docker.tar.gz"
