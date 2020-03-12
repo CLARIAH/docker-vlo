92e877e973c4daec5e8d7b47e54d1e5df5059978
Twan Goosen <twan@clarin.eu>
2020-03-12 10:06:38 +0100

    VLO version to 4.8.2

--- copy_data.env.sh
+++ copy_data.env.sh
@@ -1,4 +1,4 @@
-VLO_VERSION="4.8.2-beta2"
+VLO_VERSION="4.8.2"
 REMOTE_RELEASE_URL="https://github.com/clarin-eric/VLO/releases/download/${VLO_VERSION}/vlo-${VLO_VERSION}-docker.tar.gz"
 NAME="vlo-${VLO_VERSION}-docker"
 VLO_DISTR_FILE="webapp/vlo-${VLO_VERSION}-docker.tar.gz"
