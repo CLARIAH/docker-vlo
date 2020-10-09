36c8417ddc7286a86c040332ada356fdb9fb3b6b
Twan Goosen <twan@clarin.eu>
2020-10-09 16:27:32 +0300

    custom healthcheck in line with new procedure implemented by base image

--- copy_data.sh
+++ copy_data.sh
@@ -16,11 +16,10 @@ init_data () {
 #     fi
 
     echo -n "Fetching remote data from ${REMOTE_RELEASE_URL}... "
-    DOWNLOAD_FILE="vlo.$(date +%Y%m%d%H%M%S).tar.gz"
     (
 		cd webapp
 		wget -q -O "${VLO_DISTR_FILE}" "${REMOTE_RELEASE_URL}"
-		tar -zxf "${DOWNLOAD_FILE}"
+		tar -zxf "${VLO_DISTR_FILE}"
 		(
 			cd "${NAME}/war"
 			sh unpack-wars.sh > /dev/null
