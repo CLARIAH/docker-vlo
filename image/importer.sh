d02c12efed0d4652d5cbe446d0676fb5c8d7cc96
Twan Goosen <twan@clarin.eu>
2020-12-24 17:35:03 +0100

    renderizer template approach for all properties files for post-import utilities

--- image/importer.sh
+++ image/importer.sh
@@ -3,13 +3,8 @@ IMPORTER_SCRIPT_LOG_FILE="/var/log/vlo-import-script.log"
 
 touch $IMPORTER_SCRIPT_LOG_FILE
 
-STATS_CONFIG_BASE="/opt/vlo/bin/statistics/config"
-STATS_CONFIG_FILE="${STATS_CONFIG_BASE}.properties"
-STATS_CONFIG_FILTERED="${STATS_CONFIG_BASE}.filtered.properties"
-
+STATS_CONFIG_FILE="/opt/vlo/bin/statistics/config.properties"
 SITEMAP_CONFIG_FILE="/opt/vlo/bin/sitemap-generator/config.properties"
-SITEMAP_CONFIG_BACKUP="${SITEMAP_CONFIG_FILE}.orig"
-
 VLO_MONITOR_PROPERTIES_FILE="/opt/vlo/bin/monitor/vlo-monitor.properties"
 
 #Update mapping definitions
@@ -29,13 +24,9 @@ nice sh vlo_solr_importer.sh -c "${VLO_DOCKER_CONFIG_FILE}" > "${IMPORTER_SCRIPT
 #Solr index statistics
 if [ -n "${VLO_DOCKER_STATSD_HOST}" ] && [ -n "${VLO_DOCKER_STATSD_PORT}" ] && [ -n "${STATSD_PREFIX}" ]; then
 	# Filter properties in statics configuration
-	cp "${STATS_CONFIG_FILE}" "${STATS_CONFIG_FILTERED}"
-	replaceVarInFile "VLO_DOCKER_STATSD_HOST" "${VLO_DOCKER_STATSD_HOST}" "${STATS_CONFIG_FILTERED}"
-	replaceVarInFile "VLO_DOCKER_STATSD_PORT" "${VLO_DOCKER_STATSD_PORT}" "${STATS_CONFIG_FILTERED}"
-	replaceVarInFile "STATSD_PREFIX" "${STATSD_PREFIX}" "${STATS_CONFIG_FILTERED}"
-	# Generate statistics
-	(cd /opt/vlo/bin/statistics/ \
-		&& nice sh start.sh "${STATS_CONFIG_FILTERED}" >> "${IMPORTER_SCRIPT_LOG_FILE}" 2>&1)
+	(renderizer "${STATS_CONFIG_FILE}.tmpl" > "${STATS_CONFIG_FILE}" \
+		&& cd /opt/vlo/bin/statistics/ \
+		&& nice sh start.sh "${STATS_CONFIG_FILE}" >> "${IMPORTER_SCRIPT_LOG_FILE}" 2>&1)
 else
 	echo "WARNING: Not generating statistics - one or more required environment variables (VLO_DOCKER_STATSD_HOST, VLO_DOCKER_STATSD_PORT, STATSD_PREFIX) not set" \
 		| tee -a "${IMPORTER_SCRIPT_LOG_FILE}"
@@ -44,9 +35,9 @@ fi
 # VLO change monitor
 if [ -n "${VLO_DOCKER_MONITOR_RULES_FILE}" ]; then
 	# Filter properties in monitor configuration
-	renderizer "${VLO_MONITOR_PROPERTIES_FILE}.tmpl" > "${VLO_MONITOR_PROPERTIES_FILE}"
-	(cd /opt/vlo/bin/monitor \
-		&& nice sh start.sh 2>&1)
+	(renderizer "${VLO_MONITOR_PROPERTIES_FILE}.tmpl" > "${VLO_MONITOR_PROPERTIES_FILE}" \
+		&& cd /opt/vlo/bin/monitor \
+		&& nice sh start.sh >> "${IMPORTER_SCRIPT_LOG_FILE}" 2>&1)
 else
 	echo "Warning: not running VLO monitor because required environment variable 'VLO_DOCKER_MONITOR_RULES_FILE' has not beeen set" \
 		| tee -a "${IMPORTER_SCRIPT_LOG_FILE}"
@@ -55,18 +46,9 @@ fi
 #Sitemap
 if [ -n "${VLO_DOCKER_PUBLIC_HOME_URL}" ] && [ -n "${VLO_DOCKER_SOLR_URL}" ]; then
 	# Filter properties in sitemap generator configuraiton
-	cp "${SITEMAP_CONFIG_FILE}" "${SITEMAP_CONFIG_BACKUP}"
-	replaceVarInFile "VLO_DOCKER_PUBLIC_HOME_URL" "${VLO_DOCKER_PUBLIC_HOME_URL}" "${SITEMAP_CONFIG_FILE}"
-	replaceVarInFile "VLO_DOCKER_SOLR_URL" "${VLO_DOCKER_SOLR_URL}" "${SITEMAP_CONFIG_FILE}"
-	replaceVarInFile "VLO_DOCKER_SOLR_USER_READ_ONLY" "${VLO_DOCKER_SOLR_USER_READ_ONLY}" "${SITEMAP_CONFIG_FILE}"
-	replaceVarInFile "VLO_DOCKER_SOLR_PASSWORD_READ_ONLY" "${VLO_DOCKER_SOLR_PASSWORD_READ_ONLY}" "${SITEMAP_CONFIG_FILE}"
-
-	# Generate sitemap
-	(cd /opt/vlo/bin/sitemap-generator/ \
+	(renderizer "${SITEMAP_CONFIG_FILE}.tmpl" > "${SITEMAP_CONFIG_FILE}"
+		&& cd /opt/vlo/bin/sitemap-generator/ \
 		&& nice sh start.sh >> "${IMPORTER_SCRIPT_LOG_FILE}" 2>&1)
-
-	# Undo filtering
-	cp "${SITEMAP_CONFIG_BACKUP}" "${SITEMAP_CONFIG_FILE}"
 else
 	echo "WARNING: Not generating sitemap - one or more required environment variables (VLO_DOCKER_PUBLIC_HOME_URL, VLO_DOCKER_SOLR_URL not set" \
 		| tee -a "${IMPORTER_SCRIPT_LOG_FILE}"
