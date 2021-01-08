ed9cfb6777c039aac334bea16500c6fbdbe4ca38
Twan Goosen <twan@clarin.eu>
2021-01-08 11:49:38 +0100

    using prepared version (4.10.0-alhpa2) of the VLO bundle

--- copy_data.env.sh
+++ copy_data.env.sh
@@ -1,5 +1,4 @@
-VLO_VERSION="4.10.0-alpha1"
-#REMOTE_RELEASE_URL="https://github.com/clarin-eric/VLO/releases/download/${VLO_VERSION}/vlo-${VLO_VERSION}-docker.tar.gz"
-REMOTE_RELEASE_URL="https://github.com/twagoo/VLO/releases/download/${VLO_VERSION}/vlo-${VLO_VERSION}-docker.tar.gz"
+VLO_VERSION="4.10.0-alpha2"
+REMOTE_RELEASE_URL="https://github.com/clarin-eric/VLO/releases/download/${VLO_VERSION}/vlo-${VLO_VERSION}-docker.tar.gz"
 NAME="vlo-${VLO_VERSION}-docker"
 
