ab42ee7cdae059c6c6d5e5568daab4234545244e
Twan Goosen <twan@clarin.eu>
2021-03-10 15:25:32 +0100

    new version of vlo + new solr for testing

--- copy_data.env.sh
+++ copy_data.env.sh
@@ -1,4 +1,3 @@
-VLO_VERSION="4.10.0-alpha8"
+VLO_VERSION="4.10.0-alpha9"
 REMOTE_RELEASE_URL="https://github.com/clarin-eric/VLO/releases/download/${VLO_VERSION}/vlo-${VLO_VERSION}-docker.tar.gz"
 NAME="vlo-${VLO_VERSION}-docker"
-
