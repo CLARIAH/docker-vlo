d7b8db85ff8f060a9b65e73dfbd94522e92de66d
Twan Goosen <twan@clarin.eu>
2021-07-05 10:56:21 +0300

    append date to importer log file

--- image/importer.sh
+++ image/importer.sh
@@ -12,14 +12,14 @@ if [ -n "${VLO_MAPPING_DEFINITIONS_DIST_URL}" ]; then
 	cd "${VLO_MAPPING_DEFINITIONS_DIR}" && \
 	curl -L "${VLO_MAPPING_DEFINITIONS_DIST_URL}" | tar zxvf - --strip-components=1
 else
-	echo "Not retrieving (updated) VLO mapping definitions!" > "${IMPORTER_SCRIPT_LOG_FILE}"
+	echo "Not retrieving (updated) VLO mapping definitions!" >> "${IMPORTER_SCRIPT_LOG_FILE}"
 fi
 
 #Run importer
 export IMPORTER_JAVA_OPTS="{{.env.VLO_DOCKER_IMPORTER_JAVA_OPTS}}"
 export IMPORTER_LOG_LEVEL="${VLO_DOCKER_IMPORTER_LOG_LEVEL}"
 cd /opt/vlo/bin/ && \
-nice sh vlo_solr_importer.sh -c "${VLO_DOCKER_CONFIG_FILE}" > "${IMPORTER_SCRIPT_LOG_FILE}" 2>&1
+nice sh vlo_solr_importer.sh -c "${VLO_DOCKER_CONFIG_FILE}" >> "${IMPORTER_SCRIPT_LOG_FILE}" 2>&1
 
 #Solr index statistics
 if [ -n "${VLO_DOCKER_STATSD_HOST}" ] && [ -n "${VLO_DOCKER_STATSD_PORT}" ] && [ -n "${STATSD_PREFIX}" ]; then
@@ -53,3 +53,7 @@ else
 	echo "WARNING: Not generating sitemap - one or more required environment variables (VLO_DOCKER_PUBLIC_HOME_URL, VLO_DOCKER_SOLR_URL not set" \
 		| tee -a "${IMPORTER_SCRIPT_LOG_FILE}"
 fi
+
+#Rotate
+cat "${IMPORTER_SCRIPT_LOG_FILE}" >> "${IMPORTER_SCRIPT_LOG_FILE}.$(date +'%Y-%m-%d')" \
+	&& rm "${IMPORTER_SCRIPT_LOG_FILE}"
