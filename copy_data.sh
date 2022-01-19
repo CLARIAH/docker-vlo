69ef097e943e6b2487c8c9d4cdc68a9a04222304
Twan Goosen <twan@clarin.eu>
2022-01-19 10:02:19 +0100

    Multiarch build using new base image, new build script

--- copy_data.sh
+++ copy_data.sh
@@ -6,15 +6,12 @@ VLO_DISTR_DIR="webapp/vlo"
 VLO_DISTR_FILE="vlo-distr.$(date +%Y%m%d%H%M%S).tar.gz"
 
 init_data () { 
+	echo "Preparing to copy data for VLO image build"
+
 	export INIT_DATA_BUILD_DIR="${PWD}"
 	if [ -e "${VLO_DISTR_DIR}" ]; then
 		cleanup_data
 	fi
-
-#     LOCAL=0
-#     if [ "$1" == "local" ]; then
-#         LOCAL=1
-#     fi
 
 	(
 		cd webapp || exit 1
