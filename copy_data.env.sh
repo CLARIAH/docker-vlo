86882b322c92abe5ef15ca1aa75eb993717761ac
Twan Goosen <twan@clarin.eu>
2022-09-15 09:33:46 +0300

    VLO version to 4.11.0-alpha4

--- copy_data.env.sh
+++ copy_data.env.sh
@@ -1,3 +1,3 @@
-VLO_VERSION="4.11.0-alpha3"
+VLO_VERSION="4.11.0-alpha4"
 REMOTE_RELEASE_URL="https://github.com/clarin-eric/VLO/releases/download/${VLO_VERSION}/vlo-${VLO_VERSION}-docker.tar.gz"
 NAME="vlo-${VLO_VERSION}-docker"
