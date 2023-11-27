f0c88fc022d3f20e5b9c3b8fd400e743ac60451e
Twan Goosen <twan@clarin.eu>
2023-11-27 16:08:19 +0200

    dev copy data: vlo version

--- copy_data_dev.env.sh
+++ copy_data_dev.env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #shellcheck disable=SC2034
-VLO_VERSION="4.10.1-alpha-SNAPSHOT"
+VLO_VERSION="4.12-SNAPSHOT"
 REMOTE_RELEASE_URL="file:/Users/twagoo/git/VLO/vlo-distribution/target/vlo-${VLO_VERSION}-docker.tar.gz"
 NAME="vlo-${VLO_VERSION}-docker"
 #VLO_DISTR_FILE="webapp/vlo-${VLO_VERSION}-docker.tar.gz"
