5ce5d6ca409c26e6dfe28aa57b36dd14b8d3708d
Twan Goosen <twan@clarin.eu>
2023-12-21 14:33:11 +0100

    bumped VLO version and base image

--- copy_data.env.sh
+++ copy_data.env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #shellcheck disable=SC2034
-VLO_VERSION="4.11.6-alpha4"
+VLO_VERSION="4.11.6"
 REMOTE_RELEASE_URL="https://github.com/clarin-eric/VLO/releases/download/${VLO_VERSION}/vlo-${VLO_VERSION}-docker.tar.gz"
 NAME="vlo-${VLO_VERSION}-docker"
